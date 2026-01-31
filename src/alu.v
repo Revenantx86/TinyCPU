f1cd321b2994ed1fd2e7471e71f67cfcb6f3cdff
Taha Selim Yılmaz <tselimyilmaz@gmail.com>
2026-01-31 22:05:54 +0300

    selimtest1
    selim123

--- src/alu.v
+++ src/alu.v
@@ -7,15 +7,15 @@ module alu (
 );
 always @(*) begin
     case (opcode)
-        3*b000: result = operand_a + operand_b
-        3*b001: result = operand_a - operand_b
-        3*b010: result = operand_a & operand_b
-        3*b011: result = operand_a | operand_b
-        default: result = 8*b0;
+        3'b000: result = operand_a + operand_b
+        3'b001: result = operand_a - operand_b
+        3'b010: result = operand_a & operand_b
+        3'b011: result = operand_a | operand_b
+        default: result = 8'b0;
     endcase  
 end
 
-// Simple ALU implementation
+// Simple ALU implementationdasd
 
     // Zero flag is high if result is 0
     assign zero = (result == 8'b0);
