91d57d7b24c52568d8381871dd6e8fd7dcd86a61
Refik Yalcin <ryalcin@uwaterloo.ca>
2026-01-31 14:19:13 -0500

    program counter

--- src/pc.v
+++ src/pc.v
@@ -6,6 +6,16 @@ module pc (
     output reg  [7:0] pc_out
 );
 
-
+    always @(posedge clk) begin 
+        if(reset) begin 
+            pc_out <= 0;
+        end
+        else if(jump_en) begin
+            pc_out <= jump_addr;
+        end
+        else begin 
+            pc_out <= pc_out + 1;
+        end
+    end
 
 endmodule
